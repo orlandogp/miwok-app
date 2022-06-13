5fea46891f0c53f83f255812316300cea62693e8
Orlando Gomez <xbox_live-@live.com>
2022-06-13 17:57:17 -0500

    Version 2

--- app/src/main/java/com/example/miwok/MainActivity.java
+++ app/src/main/java/com/example/miwok/MainActivity.java
@@ -1,62 +1,38 @@
 package com.example.miwok;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
+
+import com.google.android.material.tabs.TabLayout;
 
 public class MainActivity extends AppCompatActivity {
-	
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		
-		// Set the content of the activity to use the activity_main.xml layout file
-		setContentView(R.layout.activity_main);
-		
-		// Find the View that shows the numbers, family, colors & phrases category
-		TextView numbers = findViewById(R.id.numbers);
-		TextView family = findViewById(R.id.family);
-		TextView colors = findViewById(R.id.colors);
-		TextView phrases = findViewById(R.id.phrases);
-		
-		// Set a click listener on that View
-		numbers.setOnClickListener(new View.OnClickListener() {
-			// The code in this method will be executed when the numbers View is clicked on.
-			@Override
-			public void onClick(View view) {
-				Intent numbersIntent = new Intent(MainActivity.this, NumbersActivity.class);
-				startActivity(numbersIntent);
-			}
-		});
-		// Set a click listener on that View
-		family.setOnClickListener(new View.OnClickListener() {
-			// The code in this method will be executed when the numbers View is clicked on.
-			@Override
-			public void onClick(View view) {
-				Intent familyIntent = new Intent(MainActivity.this, FamilyActivity.class);
-				startActivity(familyIntent);
-			}
-		});
-		// Set a click listener on that View
-		colors.setOnClickListener(new View.OnClickListener() {
-			// The code in this method will be executed when the numbers View is clicked on.
-			@Override
-			public void onClick(View view) {
-				Intent colorsIntent = new Intent(MainActivity.this, ColorsActivity.class);
-				startActivity(colorsIntent);
-			}
-		});
-		// Set a click listener on that View
-		phrases.setOnClickListener(new View.OnClickListener() {
-			// The code in this method will be executed when the numbers View is clicked on.
-			@Override
-			public void onClick(View view) {
-				Intent phrasesIntent = new Intent(MainActivity.this, PhrasesActivity.class);
-				startActivity(phrasesIntent);
-			}
-		});
-	}
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // Set the content of the activity to use the activity_main.xml layout file
+        setContentView(R.layout.activity_main);
+
+        // Find the view pager that will allow the user to swipe between fragments
+        ViewPager viewPager = (ViewPager) findViewById(R.id.viewpager);
+
+        // Create an adapter that knows which fragment should be shown on each page
+        CategoryAdapter adapter = new CategoryAdapter(getApplicationContext(), getSupportFragmentManager());
+
+        // Set the adapter onto the view pager
+        viewPager.setAdapter(adapter);
+
+        // Find the tab layout that shows the tabs
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.tabs);
+
+        // Connect the tab layout with the view pager. This will
+        //   1. Update the tab layout when the view pager is swiped
+        //   2. Update the view pager when a tab is selected
+        //   3. Set the tab layout's tab names with the view pager's adapter's titles
+        //      by calling onPageTitle()
+        tabLayout.setupWithViewPager(viewPager);
+    }
 }
--- app/src/main/java/com/example/miwok/WordAdapter.java
+++ app/src/main/java/com/example/miwok/WordAdapter.java
@@ -1,6 +1,7 @@
 package com.example.miwok;
 
 import android.app.Activity;
+import android.graphics.Color;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -12,58 +13,57 @@ import androidx.core.content.ContextCompat;
 
 import java.util.ArrayList;
 
-public class WordAdapter extends ArrayAdapter <Word> {
-	private static final String LOG_TAG = WordAdapter.class.getSimpleName();
-	private int mColorResourceId;
-	
-	public WordAdapter(Activity context, ArrayList <Word> word, int colorResourceId) {
-		super(context, 0, word);
-		mColorResourceId = colorResourceId;
-	}
-	
-	@Override
-	public View getView(int position, View convertView, ViewGroup parent) {
-		View listItemView = convertView;
-		if ( listItemView == null ) {
-			listItemView = LayoutInflater.from(getContext()).inflate(
-					R.layout.list_item, parent, false);
-		}
-		// Get the {@link Word} object located at this position in the list
-		Word currentWord = getItem(position);
-		
-		// Find the TextView in the list_item.xml layout with the ID miwok_text_view
-		TextView miwokTextView = (TextView) listItemView.findViewById(R.id.miwok_text_view);
-		// Get the miwok word from the current Word object and
-		// set this text on the miwok TextView
-		miwokTextView.setText(currentWord.getMiwokTranslation());
-		
-		// Find the TextView in the list_item.xml layout with the ID default_text_view
-		TextView defaultTextView = (TextView) listItemView.findViewById(R.id.default_text_view);
-		// Get the default word from the current Word object and
-		// set this text on the default TextView
-		defaultTextView.setText(currentWord.getDefaultTranslation());
-		
-		// Find the ImageView in the list_item.xml layout with the ID image
-		ImageView image = (ImageView) listItemView.findViewById(R.id.image);
-		// Get the image resource ID from the current Word object and
-		// set the image to iconView
-		if ( currentWord.hasImage() ) {
-			image.setImageResource(currentWord.getImageResourceId());
-			image.setVisibility(View.VISIBLE);
-		} else {
-			image.setVisibility(View.GONE);
-		}
-		
-		//Set the theme color fot the list item
-		View textContainer = listItemView.findViewById(R.id.text_container);
-		//Find the color that the resource ID maps to
-		int color = ContextCompat.getColor(getContext(), mColorResourceId);
-		//Set the background color for the text container view
-		textContainer.setBackgroundColor(color);
-		
-		
-		// Return the whole list item layout (containing 2 TextViews)
-		// so that it can be shown in the ListView
-		return listItemView;
-	}
+public class WordAdapter extends ArrayAdapter<Word> {
+    private static final String LOG_TAG = WordAdapter.class.getSimpleName();
+    private int mColorResourceId;
+
+    public WordAdapter(Activity context, ArrayList<Word> word, int colorResourceId){
+        super(context,0, word);
+        mColorResourceId = colorResourceId;
+    }
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        View listItemView = convertView;
+        if(listItemView == null) {
+            listItemView = LayoutInflater.from(getContext()).inflate(
+                    R.layout.list_item, parent, false);
+        }
+        // Get the {@link Word} object located at this position in the list
+        Word currentWord = getItem(position);
+
+        // Find the TextView in the list_item.xml layout with the ID miwok_text_view
+        TextView miwokTextView = (TextView) listItemView.findViewById(R.id.miwok_text_view);
+        // Get the miwok word from the current Word object and
+        // set this text on the miwok TextView
+        miwokTextView.setText(currentWord.getMiwokTranslation());
+
+        // Find the TextView in the list_item.xml layout with the ID default_text_view
+        TextView defaultTextView = (TextView) listItemView.findViewById(R.id.default_text_view);
+        // Get the default word from the current Word object and
+        // set this text on the default TextView
+        defaultTextView.setText(currentWord.getDefaultTranslation());
+
+        // Find the ImageView in the list_item.xml layout with the ID image
+        ImageView image = (ImageView) listItemView.findViewById(R.id.image);
+        // Get the image resource ID from the current Word object and
+        // set the image to iconView
+        if (currentWord.hasImage()){
+            image.setImageResource(currentWord.getImageResourceId());
+            image.setVisibility(View.VISIBLE);
+        } else {
+            image.setVisibility(View.GONE);
+        }
+
+        //Set the theme color fot the list item
+        View textContainer = listItemView.findViewById(R.id.text_container);
+        //Find the color that the resource ID maps to
+        int color = ContextCompat.getColor(getContext(), mColorResourceId);
+        //Set the background color for the text container view
+        textContainer.setBackgroundColor(color);
+
+
+        // Return the whole list item layout (containing 2 TextViews)
+        // so that it can be shown in the ListView
+        return listItemView;
+    }
 }
